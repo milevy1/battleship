116c050bd79d46dcd6be8c46285d0b23d6f77b36
Matt Levy <milevy1@gmail.com>
2019-01-29 15:17:47 -0700

    Added board test that one ship can occupy multiple cells

--- tests/board_test.rb
+++ tests/board_test.rb
@@ -3,7 +3,6 @@ require 'minitest/pride'
 require './lib/ship'
 require './lib/cell'
 require './lib/board'
-# Testing pushing to Williams pull request
 
 class CellTest < Minitest::Test
 
@@ -79,6 +78,14 @@ class CellTest < Minitest::Test
   def test_valid_placement_for_valid_cells
     assert_equal true, @board.valid_placement?(@submarine, ["A1", "A2"])
     assert_equal true, @board.valid_placement?(@cruiser, ["B1", "C1", "D1"])
+  end
+
+  def test_one_ship_can_occupy_multiple_cells
+    @board.place(@cruiser, ["A1", "A2", "A3"])
+    cell_1 = @board.cells["A1"]
+    cell_2 = @board.cells["A2"]
+    cell_3 = @board.cells["A3"]
+    assert_equal cell_3.ship, cell_2.ship
   end
 
 end
