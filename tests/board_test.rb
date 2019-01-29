45e19560e760034ef6b11600beb78e42fa3711e0
wipegup <wipegup@gmail.com>
2019-01-28 21:06:11 -0700

    Started Board_test

---
+++ tests/board_test.rb
@@ -0,0 +1,49 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/ship'
+require './lib/cell'
+require './lib/board'
+
+class CellTest < Minitest::Test
+
+  def setup
+    # For iteration 4
+    # @dim = 4
+
+    @board = Board.new#(@dim)
+  end
+
+  def test_it_has_correct_number_cells
+    expected_cells = 16 # @dim * @dim
+
+    cell_hash = @board.cells
+
+    assert_equal expected_cells, cell_hash.keys.length
+  end
+
+  def test_each_cell_is_a_cell
+    cell_hash = @board.cells
+
+    cell_hash.values.each { |cell| assert_instance_of Cell, cell}
+  end
+
+  def test_each_cell_coordinate_in_board
+    dim = 4
+    columns = (1..(dim)).to_a
+    rows = (65.chr..(65+dim-1).chr).to_a
+
+    cell_hash = @board.cells
+
+    cell_hash.keys.each do |coordinate|
+      assert rows.include?(coordinate[0])
+      assert columns.include?(coordinate[1..-1])
+    end
+  end
+
+  def test_place_fills_correct_cells
+    skip
+    ###
+  end
+
+
+end
