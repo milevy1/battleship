230a28ce891614d9263403708cb913afce9744f4
Matt Levy <milevy1@gmail.com>
2019-01-30 15:36:01 -0700

    Initial commit game and game test files

---
+++ tests/game_test.rb
@@ -0,0 +1,12 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/ship'
+require './lib/cell'
+require './lib/board'
+require './lib/messages'
+
+class GameTest < Minitest::Test
+
+  
+
+end
