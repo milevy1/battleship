2846b3e9d1d183e5dc168b3cc5df8064f76bca0c
Matt Levy <milevy1@gmail.com>
2019-01-29 18:55:30 -0700

    Removed skips.  Board place now passes all the tests in there

--- tests/board_test.rb
+++ tests/board_test.rb
@@ -81,7 +81,6 @@ class CellTest < Minitest::Test
   end
 
   def test_one_ship_can_occupy_multiple_cells
-    skip
     @board.place(@cruiser, ["A1", "A2", "A3"])
     cell_1 = @board.cells["A1"]
     cell_2 = @board.cells["A2"]
@@ -90,7 +89,6 @@ class CellTest < Minitest::Test
   end
 
   def test_valid_placement_checks_that_ships_do_not_overlap
-    skip
     @board.place(@cruiser, ["A1", "A2", "A3"])
 
     assert_equal false, @board.valid_placement?(@submarine, ["A1", "B1"])
