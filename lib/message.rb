5efc0fc69bea288ff6cf9bba5abc59dd06bbddf3
Matt Levy <milevy1@gmail.com>
2019-01-30 10:36:22 -0700

    Added player_ship_placement_input

--- lib/message.rb
+++ lib/message.rb
@@ -11,4 +11,8 @@ class Message
     puts "The Cruiser is two units long and the Submarine is three units long."
   end
 
+  def player_ship_placement_input(ship)
+    puts "Enter the squares for the #{ship.name} (#{ship.length} spaces):"
+  end
+
 end
