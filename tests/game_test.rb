7ff0207931299533a50380ace74c7d44e188c08e
Matt Levy <milevy1@gmail.com>
2019-01-30 15:47:14 -0700

    Added test for setup game output

--- tests/game_test.rb
+++ tests/game_test.rb
@@ -4,9 +4,18 @@ require './lib/ship'
 require './lib/cell'
 require './lib/board'
 require './lib/messages'
+require './lib/game'
 
 class GameTest < Minitest::Test
 
-  
+  def setup
+    @game = Game.new
+  end
+
+  def test_setup_game_output
+    expected = "Welcome to BATTLESHIP\nEnter p to play. Enter q to quit.\n> "
+
+    assert_output(expected) { @game.setup_game }
+  end
 
 end
