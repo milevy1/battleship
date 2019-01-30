46f99b045481a1c096c0a1c4a4b7a8a62261657f
Matt Levy <milevy1@gmail.com>
2019-01-30 15:59:22 -0700

    Added player_board and computer_board attributes

--- lib/game.rb
+++ lib/game.rb
@@ -1,13 +1,28 @@
 class Game
-  attr_reader :message
+  attr_reader :message, :player_board, :computer_board
 
   def initialize
     @message = Messages.new
+    @player_board = nil
+    @computer_board = nil
   end
 
   def main_menu
     @message.main_menu
     @message.prompt_user
+    play_input = gets.chomp.downcase
+    if play_input == "p"
+      setup_game
+    elsif play_input == "q"
+      # Quits game
+    else
+      puts "Invalid input, please try again."
+      main_menu
+    end
+  end
+
+  def setup_game
+
   end
 
 end
