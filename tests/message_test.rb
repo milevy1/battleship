5ade00f2ab32389f80147285f86ba89746f253f6
Matt Levy <milevy1@gmail.com>
2019-01-30 10:56:56 -0700

    Added test for message player ship placement method

--- tests/message_test.rb
+++ tests/message_test.rb
@@ -3,11 +3,19 @@ require 'minitest/pride'
 require './lib/ship'
 require './lib/cell'
 require './lib/board'
+require './lib/message'
 
 class MessageTest < Minitest::Test
 
   def setup
+    @message = Message.new
     @cruiser = Ship.new("Cruiser", 3)
+  end
+
+  def test_player_ship_placement_input
+    expected = "Enter the squares for the Cruiser (3 spaces):\n"
+
+    assert_output(expected) { @message.player_ship_placement_input(@cruiser) }
   end
 
 end
