b22d4748cde374ec76890ec73691735dfbe93719
Matt Levy <milevy1@gmail.com>
2019-01-30 21:16:10 -0700

    Moved prompt user into other methods

--- lib/messages.rb
+++ lib/messages.rb
@@ -3,24 +3,28 @@ class Messages
   def main_menu
     puts "Welcome to BATTLESHIP"
     puts "Enter p to play. Enter q to quit."
+    prompt_user
   end
 
   def prompt_user
     print "> "
   end
 
-  def player_ship_placement_intro
+  def player_ship_placement_intro(player_board)
     puts "I have laid out my ships on the grid."
     puts "You now need to lay out your two ships."
     puts "The Cruiser is two units long and the Submarine is three units long."
+    puts player_board.render(true)
   end
 
   def player_ship_placement_input(ship)
     puts "Enter the squares for the #{ship.name} (#{ship.length} spaces):"
+    prompt_user
   end
 
   def ship_placement_invalid_coordinates
     puts "Those are invalid coordinates. Please try again:"
+    prompt_user
   end
 
   def computer_board(computer_board)
