7cc1c59ec92c82fc67227dbbebd17efc80c63b69
Matt Levy <milevy1@gmail.com>
2019-01-30 21:17:39 -0700

    Added setup_game method. Very long, needs refactoring.

--- lib/game.rb
+++ lib/game.rb
@@ -1,15 +1,12 @@
 class Game
-  attr_reader :message, :player_board, :computer_board
+  attr_reader :message, :player_board, :computer_board, :computer_player
 
   def initialize
     @message = Messages.new
-    @player_board = nil
-    @computer_board = nil
   end
 
   def main_menu
     @message.main_menu
-    @message.prompt_user
     play_input = gets.chomp.downcase
     if play_input == "p"
       setup_game
@@ -22,7 +19,26 @@ class Game
   end
 
   def setup_game
+    @player_board = Board.new
+    player_cruiser = Ship.new("Cruiser", 3)
+    player_submarine = Ship.new("Submarine", 2)
+    player_ships = [player_cruiser, player_submarine]
+    @computer_board = Board.new
+    computer_cruiser = Ship.new("Cruiser", 3)
+    computer_submarine = Ship.new("Submarine", 2)
+    computer_ships = [computer_cruiser, computer_submarine]
+    @computer_player = ComputerPlayer.new(@player_board, @computer_board, computer_ships)
+    @computer_player.place_own_ships
 
+    @message.player_ship_placement_intro
+
+    player_ships.each { |ship|
+      @message.player_ship_placement_input(ship)
+      while !@player_board.place(ship, gets.chomp.split)
+        @message.ship_placement_invalid_coordinates
+      end
+      puts @player_board.render(true)
+     }
   end
 
 end
