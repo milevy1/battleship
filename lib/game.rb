b4d7d13a68308806694b21e3d0db54403ea8151d
Matt Levy <milevy1@gmail.com>
2019-02-01 21:24:17 -0700

    Moved the to_i from ship selection

--- lib/game.rb
+++ lib/game.rb
@@ -58,20 +58,20 @@ class Game
 
     @message.would_you_like_to_customize_ships?
     valid_selections = [1, 2, 3]
-    ship_selection = gets.chomp
+    ship_selection = gets.chomp.to_i
 
-    until valid_selections.include?(ship_selection.to_i)
+    until valid_selections.include?(ship_selection)
       @message.invalid_customize_ship_selection
-      ship_selection = gets.chomp
+      ship_selection = gets.chomp.to_i
     end
 
-    case ship_selection.to_i
+    case ship_selection
     when 1
-      return [['Cruiser',3],['Submarine',2]]
+      return [['Cruiser', 3],['Submarine', 2]]
     when 2
-      return [['Battleship',4],['Cruiser',3],['Submarine',2]]
+      return [['Battleship', 4],['Cruiser', 3],['Submarine', 2]]
     when 3
-      return [['Carrier', 5],['Battleship',4],['Cruiser',3],['Submarine',2]]
+      return [['Carrier', 5],['Battleship', 4],['Cruiser', 3],['Submarine', 2]]
     end
 
   end
