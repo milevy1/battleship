e161dedddfdedfe9d9421ecc9cbb4758cbabdb26
Matt Levy <milevy1@gmail.com>
2019-02-03 08:37:19 -0700

    Shortened the user_ship_total_length method

--- lib/game.rb
+++ lib/game.rb
@@ -97,7 +97,7 @@ class Game
     user_ships = []
 
     until user_input == "N"
-      user_ships_total_length = user_ships.map { |ship| ship[1] }.inject(0){ |sum, x| sum + x }
+      user_ships_total_length = user_ships.sum{ |ship| ship[1]}
       ship_name  = @message.prompt_user_for_custom_ship_name(user_ships, user_ships_total_length, board_area)
       ship_length = @message.prompt_user_for_custom_ship_length(ship_name, user_ships_total_length, board_area, rows, columns)
 
