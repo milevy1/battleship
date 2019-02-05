510eda87d75d8195e3a0350f7fefc1966923c0d5
wipegup <wipegup@gmail.com>
2019-02-05 10:51:27 -0700

    Remove comments

--- lib/cell.rb
+++ lib/cell.rb
@@ -36,6 +36,7 @@ class Cell
       else
         return "."
       end
+
     else # There is a ship
       if @fired_upon
         if @ship.sunk?
@@ -43,6 +44,7 @@ class Cell
         else
           return "H"
         end
+
       else # Not fired upon
         if debug
           return "S"
@@ -51,13 +53,5 @@ class Cell
         end
       end
     end
-
-    # return "H" if @ship != nil && @fired_upon == true
-    # return "S" if @ship != nil && debug == true
-    # return "." if @fired_upon == false
-    # return "M" if @ship == nil && @fired_upon == true
-    # return "X" if @ship != nil && @ship.sunk?
-
-
   end
 end
