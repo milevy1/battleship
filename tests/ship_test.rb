e2b7e74b685b022f7b83fb154fff2dd6cac9be84
wipegup <wipegup@gmail.com>
2019-02-05 10:54:19 -0700

    Move initial sunk test

--- tests/ship_test.rb
+++ tests/ship_test.rb
@@ -16,11 +16,10 @@ class ShipTest < Minitest::Test
     assert_equal "Cruiser", @cruiser.name
     assert_equal 3, @cruiser.length
     assert_equal 3, @cruiser.health
+    assert_equal false, @cruiser.sunk?
   end
 
   def test_ship_attributes_change_after_hits
-    assert_equal false, @cruiser.sunk?
-
     @cruiser.hit
     assert_equal 2, @cruiser.health
     assert_equal false, @cruiser.sunk?
